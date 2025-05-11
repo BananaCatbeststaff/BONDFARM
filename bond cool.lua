d2f684d7fc5915ce6196cf87031c06c8ab70c65e
Founder_on <galinha.chata.muito.chata@gmail.com>
2025-05-10 23:28:56 -0300

    Update bond cool.lua

--- bond cool.lua
+++ bond cool.lua
@@ -1,1 +1,14 @@
-print("nothing here LMAOAOAO")
+local TeleportService = game:GetService("TeleportService")
+local Players = game:GetService("Players")
+
+wait(60)
+local function hop()
+    local success, err = pcall(function()
+        TeleportService:Teleport(game.PlaceId, Players.LocalPlayer)
+    end)
+    if not success then
+        warn("Erro ao tentar teletransportar:", err)
+    end
+end
+
+hop()
